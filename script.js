8eedcef56a41234bf32a125b04ef090811fa74f2
Cael <caeltindoy@gmail.com>
2026-07-23 00:28:07 +0800

    Update list text style

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ let list = document.querySelector('#list-container');
 
 shoppingForm.addEventListener('submit', function(e) {
 	e.preventDefault();
+
 	let task = input.value;
 	let li = document.createElement('li');
-	li.textContent = task;
+	let span = document.createElement('span');
+	span.textContent = task;
+
+	let numberList = document.querySelectorAll('#list-container li').length + 1;
+
 
 	if (task !== '') { 
+		li.id = 'task' + numberList;
+		li.appendChild(span);
 		list.appendChild(li); 
 		input.value = '';
 	}
